cdc4326ff69eb7b08c0fbb5921418e3c4f8f0674
KaterinaKN89 <nedyalkovakaterina@gmail.com>
2023-09-12 17:53:44 +0300

    Add Trello Web Driver Homework

--- SeleniumWebDriverHomework/src/test/java/searchtests/BingSearchByTermTest.java
+++ SeleniumWebDriverHomework/src/test/java/searchtests/BingSearchByTermTest.java
@@ -4,11 +4,7 @@ import basetest.BaseTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
 
 public class BingSearchByTermTest extends BaseTest {
 
